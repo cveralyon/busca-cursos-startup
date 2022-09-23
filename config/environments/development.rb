f7e0530c59f2238017ce01aea73103bd349880dd
cveralyon <cavera2@miuandes.cl>
2022-09-23 17:08:33 -0300

    funciondo en heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.hosts << "busca-clases-startup.herokuapp.com"
+
+
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
 
